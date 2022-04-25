e401dfe0ba453e762e2fb2baebd5424f0e2b7870
PhucLQ <lequyphuc@luvina.net>
2022-04-25 14:33:35 +0700

    first commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static final String name = "phuc";
+    public String age ;
+    public Demo(String age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
